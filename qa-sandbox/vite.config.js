fea1b63bce3d08805db9dc9ce152fcf4d2a58d09
Dylan Le <68560708+ExoticButters15@users.noreply.github.com>
2026-04-04 17:53:46 -0700

    fixed tunnel url

--- qa-sandbox/vite.config.js
+++ qa-sandbox/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    allowedHosts: ['href-networks-seemed-robust.trycloudflare.com/']
+    allowedHosts: ['href-networks-seemed-robust.trycloudflare.com']
   }
 })
